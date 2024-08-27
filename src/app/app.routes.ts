d62eec6ecf7e11305f4984dadaa33bd31be26526
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-08-27 21:06:27 +0200

    designed startscreen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {path: '', component: StartScreenComponent}
+];
